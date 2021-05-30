dd6f7bfeab07fb68604b4ed057beda574eedeb45
TEDIORELEE <tediorelee@qq.com>
2021-05-31 01:08:30 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="q-app">
+		<q-ajax-bar/>
+		<transition
+			enter-active-class="animated fadeIn"
+			leave-active-class="animated fadeOut"
+			mode="out-in"
+		>
+			<keep-alive>
+				<router-view />
+			</keep-alive>
+		</transition>
+	</div>
+</template>
+<script>
+export default {
+	name: 'App'
+};
+</script>
